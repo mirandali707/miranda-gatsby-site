390cf7c9fc1a4afe604200c84a44d9f6472738b3
Miranda <mirandal@stanford.edu>
2023-09-04 12:27:50 -0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Hi, this is Miranda's website.
-
-Hi Miranda, `npm run deploy` is probably what you're looking for.
--- src/styles/mirandaConfig.tsx
+++
@@ -1,6 +0,0 @@
-export const mirandaConfig = {
-    "token": {
-        "colorPrimary": "#cf4040",
-        "colorInfo": "#cf4040"
-    },
-}
