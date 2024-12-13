9d1ea66a7a2eb25de6a9c8390f0ee082660a3ccd
Miranda <mirandal@stanford.edu>
2024-12-13 15:55:56 -0800

    bug fix -- undefined tag option for some reason

--- src/components/TagFilterSelect.tsx
+++ src/components/TagFilterSelect.tsx
@@ -27,10 +27,16 @@ const TagFilterSelect = ({ setActiveTags }) => {
 
 const tagRender = (props: CustomTagProps) => {
     const { label, value, closable, onClose } = props;
+
+    if (!value) {
+      return <></>;
+    }
+
     const onPreventMouseDown = (event: React.MouseEvent<HTMLSpanElement>) => {
       event.preventDefault();
       event.stopPropagation();
     };
+
     return (
     <Tooltip placement="top" title={tagProperties[value].text}>
       <Tag
